b1722d90ca68743fdefa38698e07d767db2bc310
chalme <pc.chalme@gmail.com>
2024-01-02 18:25:17 +0800

    feat: transfer mongo to mysql

--- transfer/src/main.rs
+++ transfer/src/main.rs
@@ -1,9 +1,6 @@
 use chrono::NaiveDateTime;
-use std::collections::HashMap;
-
 use mongodb::{options::ClientOptions, Client};
 use mysql_async::prelude::Query;
-use mysql_common::params::Params;
 use mysql_common::prelude::FromRow;
 use mysql_common::FromRowError;
 use serde::{Deserialize, Serialize};
@@ -15,13 +12,13 @@ struct DataSheetRecord {
   dst_id: Option<String>,
   // #[mysql(json)]
   data: Option<serde_json::Value>,
-  revision_history: String,
+  revision_history: Option<String>,
   revision: Option<u64>,
   // // #[mysql(json)]
   field_updated_info: Option<serde_json::Value>,
   is_deleted: u8,
-  created_by: Option<u64>,
-  updated_by: Option<u64>,
+  created_by: Option<i64>,
+  updated_by: Option<i64>,
   // created_at: Option<Vec<u8>>,
   // updated_at: Option<Vec<u8>>,
   created_at: Option<NaiveDateTime>,
@@ -47,25 +44,26 @@ async fn main() -> anyhow::Result<()> {
   let collection_name = "vika_datasheet_record";
 
   // Delete the collection
-  match mongodb_db
-    .collection::<DataSheetRecord>(collection_name)
-    .drop(None)
-    .await
-  {
-    Ok(_) => println!("Collection '{}' deleted successfully", collection_name),
-    Err(e) => eprintln!("Error deleting collection: {}", e),
-  }
+  // match mongodb_db
+  //   .collection::<DataSheetRecord>(collection_name)
+  //   .drop(None)
+  //   .await
+  // {
+  //   Ok(_) => println!("Collection '{}' deleted successfully", collection_name),
+  //   Err(e) => eprintln!("Error deleting collection: {}", e),
+  // }
 
   // 分页参数
   let page_size = 10000;
-  let mut last_id = 1917756202075178;
+  let mut last_id = 0;
 
-  let mut conn = mysql_pool.get_conn().await?;
-  loop {
-    // let sql = format!("SELECT * FROM vika_datasheet_record where dst_id=:dst_id LIMIT {} OFFSET {}", page_size, offset);
+  /// 1466343107857416195
+  for _ in 0..10 {
+    let mut conn = mysql_pool.get_conn().await?;
+    // let sql = format!("SELECT * FROM vika_datasheet_record where id > {} and dst_id='dstrmVd9p6ZPMYXbXc' order by id LIMIT {}", last_id, page_size);
     // let param = params! {"dst_id" => "dstrmVd9p6ZPMYXbXc"};
     let sql = format!("SELECT * FROM vika_datasheet_record where id > {} and created_at != '0000-00-00 00:00:00' and updated_at != '0000-00-00 00:00:00' order by id LIMIT {}", last_id, page_size);
-    let param = Params::Named(HashMap::new());
+    // let param = Params::Named(HashMap::new());
 
     println!("conn get, sql:{}", sql);
     let rows: Vec<DataSheetRecord> = sql
@@ -73,18 +71,21 @@ async fn main() -> anyhow::Result<()> {
       .fetch(&mut conn)
       .await?;
     // 从 MySQL 获取数据
-
     last_id = rows.last().map(|row| row.id).unwrap_or(0);
     // 如果没有更多的行，退出循环
     if rows.is_empty() {
       break;
     }
     // break;
+    println!("{}", rows.len());
 
-    mongodb_db
-      .collection::<DataSheetRecord>("vika_datasheet_record")
+    let result = mongodb_db
+      .collection::<DataSheetRecord>(collection_name)
       .insert_many(rows, None)
-      .await?;
+      .await;
+    if result.is_err() {
+      println!("insert_many error: {:?}", result);
+    }
   }
   Ok(())
 }
